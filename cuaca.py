b9ea59d8ddc6e81d953b784255c61426d8e77471
insomniacyberteam <45147248+insomniacyberteam@users.noreply.github.com>
2019-04-05 22:06:02 +0700

    Add files via upload

---
+++ cuaca.py
@@ -0,0 +1,335 @@
+#!/usr/bin/python
+#Coded By ExGeneralTz
+import os
+import time
+
+blue = '\033[34;1m'
+green = '\033[32;1m'
+purple = '\033[35;1m'
+cyan = '\033[36;1m'
+red = '\033[31;1m'
+white = '\033[37;1m'
+yellow = '\033[33;1m'
+
+os.system("clear")
+print ("\033[36;1m####################################################")
+print ("\033[36;1m#\033[31;1m Author\033[37;1m : ExGeneralTz                             \033[36;1m#")
+print ("\033[36;1m#\033[31;1m Team\033[37;1m : Insomnia Cyber Team                       \033[36;1m#")
+print ("\033[36;1m#\033[31;1m Tools Tipe\033[37;1m : Prediksi Cuaca Di Pulau Jawa        \033[36;1m#")
+print ("\033[36;1m#\033[31;1m Web Kami\033[37;1m : https://www.insomniacyberteam.zone.id \033[36;1m#")
+print ("\033[36;1m####################################################")
+os.system("date")
+print ("\033[31;1m")
+print ("\033[32;1mPilih Sesuai Lokasi Anda :) ")
+print ("\033[37;1m1>>\033[36;1mJakarta ")
+print ("\033[37;1m2>>\033[36;1mBogor ")
+print ("\033[37;1m3>>\033[36;1mTanggerang ")
+print ("\033[37;1m4>>\033[36;1mSurabaya ")
+print ("\033[37;1m5>>\033[36;1mYogyakarta ")
+print ("\033[37;1m6>>\033[36;1mBandung ")
+print ("\033[37;1m7>>\033[36;1mCirebon ")
+print ("\033[37;1m8>>\033[36;1mMalang ")
+print ("\033[37;1m9>>\033[36;1mTegal ")
+print ("\033[37;1m10>>\033[36;1mSolo ")
+print ("\033[37;1m11>>\033[36;1mSemarang ")
+print ("\033[37;1m12>>\033[36;1mJepara ")
+print ("\033[37;1m13>>\033[36;1mKudus ")
+print ("\033[37;1m14>>\033[36;1mPurwokerto ")
+print ("\033[37;1m15>>\033[36;1mGombong ")
+print ("\033[37;1m16>>\033[36;1mBanyumas ")
+print ("\033[37;1m17>>\033[36;1mBekasi ")
+print ("\033[37;1m18>>\033[36;1mBlitar ")
+print ("\033[37;1m19>>\033[36;1mSerang ")
+print ("\033[37;1m20>>\033[36;1mBanten ")
+print ("\033[37;1m21>>\033[36;1mKroya ")
+print ("\033[37;1m22>>\033[36;1mKediri ")
+print ("\033[37;1m23>>\033[36;1mJombang ")
+print ("\033[37;1m24>>\033[36;1mJember ")
+print ("\033[37;1m25>>\033[36;1mDemak ")
+print ("\033[37;1m26>>\033[36;1mKebumen ")
+print ("\033[37;1m27>>\033[36;1mCilacap ")
+print ("\033[37;1m28>>\033[36;1mPati ")
+print ("\033[37;1m29>>\033[36;1mBrebes ")
+print ("\033[37;1m30>>\033[36;1mSidoarjo ")
+print ("\033[37;1m99>>\033[36;1mExit The Program ")
+Pilih = input("[Pilih Kota] : ")
+
+if Pilih==1:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Jakarta')
+
+if Pilih==2:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Bogor')
+
+if Pilih==3:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Tangerang')
+
+if Pilih==4:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Surabaya')
+
+if Pilih==5:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Yogyakarta')
+
+if Pilih==6:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Bandung')
+
+if Pilih==7:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Cirebon')
+
+if Pilih==8:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Malang')
+
+if Pilih==9:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Tegal')
+
+if Pilih==10:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Solo')
+
+if Pilih==11:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Semarang')
+
+if Pilih==12:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Jepara')
+
+if Pilih==13:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Kudus')
+
+if Pilih==14:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Purwokerto')
+
+if Pilih==15:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Gombong')
+
+if Pilih==16:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Banyumas')
+
+if Pilih==17:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Bekasi')
+
+if Pilih==18:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Blitar')
+
+if Pilih==19:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Serang')
+
+if Pilih==20:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Banten')
+
+if Pilih==21:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Kroya')
+
+if Pilih==22:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Kediri')
+
+if Pilih==23:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Jombang')
+
+if Pilih==24:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Jember')
+
+if Pilih==25:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Demak')
+
+if Pilih==26:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Kebumen')
+
+if Pilih==27:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Cilacap')
+
+if Pilih==28:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Pati')
+
+if Pilih==29:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Brebes')
+
+if Pilih==30:
+    os.system ('clear')
+    os.system ('pkg install curl')
+    os.system ('clear')
+    time.sleep (1.5)
+    print(green),("Loading")
+    os.system ('clear')
+    os.system ('curl http://wttr.in/Sidoarjo')
+
+if Pilih==99:
+    time.sleep (1.5)
+    print(green),("Thanks To All Member Insomnia Cyber Team")
+    time.sleep (1.5)
+    print(green),("Jangan Lupa Kunjungi Web Kami")
+    time.sleep (1.5)
+    print(green),("Ane ExGeneralTz Izin Pamit")
+    time.sleep (1.5)
+    print(green),("Byee-Byee")
